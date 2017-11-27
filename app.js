42c3240f0c20f37f717f1faa66e509fd84970b69
Sreeja Gupta <sreejagpt@gmail.com>
2017-11-27 19:42:12 +1100

    Lumos! 💡

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const ROOT_DIR = './'
+const Sonus = require('sonus')
+const speech = require('@google-cloud/speech')({
+  projectId: 'streaming-speech-sample',
+  keyFilename: ROOT_DIR + 'keyfile.json'
+})
+
+const hotwords = [{ file: ROOT_DIR + 'resources/lumos.pmdl', hotword: 'lumos' }]
+const language = "en-US"
+
+//recordProgram can also be 'arecord' which works much better on the Pi and low power devices
+const sonus = Sonus.init({ hotwords, language, recordProgram: "rec" }, speech)
+
+Sonus.start(sonus)
+console.log('Say "' + hotwords[0].hotword + '"...')
+
+sonus.on('hotword', (index, keyword) => console.log('💡'))
+
+sonus.on('partial-result', result => console.log("Partial", result))
+
+sonus.on('error', error => console.log('error', error))
+
+sonus.on('final-result', result => {
+  console.log("Final", result)
+  if (result.includes("stop")) {
+    Sonus.stop()
+  }
+})

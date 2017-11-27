959b23dc6cbc7b56de8de32635aad9da76da4757
Sreeja Gupta <sreejagpt@gmail.com>
2017-11-27 20:59:31 +1100

    make requests to server on lumos

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
 
 const ROOT_DIR = './'
 const Sonus = require('sonus')
+var request = require('request')
 const speech = require('@google-cloud/speech')({
   projectId: 'streaming-speech-sample',
   keyFilename: ROOT_DIR + 'keyfile.json'
 })
+
+var options = {
+    url: 'https://hashtag-api.herokuapp.com/lumos',
+    method: 'POST'
+}
 
 const hotwords = [{ file: ROOT_DIR + 'resources/lumos.pmdl', hotword: 'lumos' }]
 const language = "en-US"
@@ -16,7 +22,15 @@ const sonus = Sonus.init({ hotwords, language, recordProgram: "rec" }, speech)
 Sonus.start(sonus)
 console.log('Say "' + hotwords[0].hotword + '"...')
 
-sonus.on('hotword', (index, keyword) => console.log('💡'))
+sonus.on('hotword', (index, keyword) => {
+  console.log('💡')
+  // Start the request
+  request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          console.log('Sent notification')
+      }
+  })
+})
 
 sonus.on('partial-result', result => console.log("Partial", result))
 
